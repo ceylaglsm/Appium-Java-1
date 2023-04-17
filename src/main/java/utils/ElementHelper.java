7ad51139a870ac63efb6741e59e17843a74c277d
Gulsum Uslu <ceylaglsm@gmail.com>
2023-04-18 02:55:52 +0300

    18.04.2023

--- src/main/java/utils/ElementHelper.java
+++ src/main/java/utils/ElementHelper.java
@@ -21,8 +21,20 @@ public class ElementHelper {
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
-   public void checkElement(By locator){
-        driver.findElement(locator);
-   }
+    public WebElement findElement(By locator) {
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+    public void checkElement(By locator) {
+        findElement(locator);
+    }
+    public void click(By locator) {
+        findElement(locator).click();
+    }
+    public String getText(By locator) {
+        return findElement(locator).getText();
+    }
 
+    public void sendKeys(By locator,String text) {
+        findElement(locator).sendKeys(text);
+    }
 }
